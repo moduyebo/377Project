c203ad3d7a0ad41b5bb655ffca6d380df2531632
michaeloduyebo <mioduyebo@gmail.com>
2019-12-04 04:00:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,6 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', function(req, res) {
-    let data = {
-        message: 'Hello World!'
-    };
-    res.status(200).send(data);
-});
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
