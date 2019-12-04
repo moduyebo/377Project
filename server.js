1bbee47fe38fdc6a3c4d6428fc0a0ece9b78b934
michaeloduyebo <mioduyebo@gmail.com>
2019-12-04 01:34:56 -0500

    idk

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
-//app.get('/', (req, res) => res.render('index'));
+app.get((req, res) => res.render('index'));
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
