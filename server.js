5c68b93cf1f73fcc4e1f669b27067fa07beb82a1
michaeloduyebo <mioduyebo@gmail.com>
2019-12-04 03:56:13 -0500

    delete public folder

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.get('/', function(req, res) {
+    let data = {
+        message: 'Hello World!'
+    };
+    res.status(200).send(data);
+});
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
